51c24e9f11b1b3728994ed0503c7ef02572a5acf
fwieder <59224157+fwieder@users.noreply.github.com>
2024-07-03 23:54:58 +0200

    add rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "fluxcones",
+    version = "1.0.0",
+    author = "Frederik Wieder",
+    description = ("The fluxcones package"),
+    url = "https://github.com/fwieder/fluxcones",
+    packages=['fluxcones'],
+    long_description=read('Readme.md'),
+)
