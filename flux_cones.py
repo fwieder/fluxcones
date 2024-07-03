d2d4d59c967417c70e3602ac2320e90eaafa731b
fwieder <59224157+fwieder@users.noreply.github.com>
2024-07-03 20:03:27 +0200

    move efm algos from helpers to cones class

--- flux_cones.py
+++ flux_cones.py
@@ -114,27 +114,163 @@ class flux_cone:
             return True
 
         return False
-    
-    
-    ''' compute the EFMs of the fluxcone using cdd, efmtool or the milp approach'''
-    
-    def get_efms(self, algo = "efmtool"):
-        if algo == "cdd":
-            efms = get_efms(self.stoich,self.rev,algo = "cdd")
+
+    def get_efms_efmtool(self):
+        """
+        initiate reaction names and metabolite names from 0 to n resp. m because
+        efmtool needs these lists of strings as input
+        "normalize options:  [max, min, norm2, squared, none]
+        """
+
+        opts = dict({
+            "kind": "stoichiometry",
+            "arithmetic": "double",
+            "zero": "1e-10",
+            "compression": "default",
+            "log": "console",
+            "level": "OFF",
+            "maxthreads": "-1",
+            "normalize": "max",
+            "adjacency-method": "pattern-tree-minzero",
+            "rowordering": "MostZerosOrAbsLexMin"
+        })
+
+        if only_reversible:
+            S = np.r_[self.stoich, np.eye(self.num_reacs)[supp(self.irr)]]
+        else:
+            S = self.stoich
+
+        reaction_names = list(np.arange(len(S[0])).astype(str))
+        metabolite_names = list(np.arange(len(S)).astype(str))
+
+        efms_cols = efmtool.calculate_efms(
+            S, self.rev, reaction_names, metabolite_names, opts)
+
+        if only_reversible:
+            self.rev_efms = efms_cols.T
+        else:
+            self.efms = efms_cols.T
+
+    def get_efms_cdd(self, only_reversible=False):
+        if only_reversible:
+            S = np.r_[self.stoich, np.eye(self.num_reacs)[supp(self.irr)]]
+        else:
+            S = self.stoich
+
+        # Store information about original shape to be able to revert splitting
+        # of reversible reactions later
+        original_shape = np.shape(S)
+        rev_indices = np.nonzero(self.rev)[0]
+
+        # split reversible reactions by appending columns
+        S_split = np.c_[S, -S[:, rev_indices]]
+
+        # compute generators of pointed cone by splitting (all reactions irreversible)
+        res = np.array(get_gens(S_split, np.zeros(len(S_split[0]))))
+
+        # reverse splitting by combining both directions that resulted from splitting
+        orig = res[:, :original_shape[1]]
+        torem = np.zeros(np.shape(orig))
+        splits = res[:, original_shape[1]:]
+
+        for i, j in enumerate(rev_indices):
+            torem[:, j] = splits[:, i]
+        unsplit = orig - torem
+        tokeep = []
+
+        # remove spurious cycles
+        for index, vector in enumerate(unsplit):
+            if len(supp(vector)) > 0:
+                tokeep.append(index)
+
+        efms = unsplit[tokeep]
+
+        if only_reversible:
+            self.rev_efms = efms
+        else:
             self.efms = efms
-            return efms
-    
-        if algo == "efmtool":
-            efms = get_efms(self.stoich,self.rev,algo = "efmtool")
+
+    def get_efms_milp(self, only_reversible=False):
+        """
+        compute the EFMs of the fluxcone using the milp approach
+
+        """
+
+        if only_reversible:
+            S = np.r_[self.stoich, np.eye(self.num_reacs)[supp(self.irr)]]
+        else:
+            S = self.stoich
+
+        # Create the extended stoichiometric matrix for reversible reactions
+        for index in np.nonzero(self.rev)[0]:
+            S = np.c_[S, -S[:, index]]
+
+        n = np.shape(S)[1]
+        # Initialize the MILP model
+        m = Model(sense=MINIMIZE)
+
+        m.verbose = False
+
+        # Add binary variables for each reaction
+        a = [m.add_var(var_type=BINARY) for _ in range(n)]
+
+        # Add continuous variables for each reaction rate
+        v = [m.add_var() for _ in range(n)]
+
+        # Add stoichiometric constraints
+        for row in S:
+            m += xsum(row[i] * v[i] for i in range(n)) == 0
+
+        # Define the Big M value for constraints
+        M = 1000
+        for i in range(n):
+            m += a[i] <= v[i]
+            m += v[i] <= M * a[i]
+
+        # Exclude the zero vector solution
+        m += xsum(a[i] for i in range(n)) >= 1
+
+        # Set the objective to minimize the number of non-zero variables
+        m.objective = xsum(a[i] for i in range(n))
+
+        efms = []
+
+        while True:
+            # Solve the MILP model
+            m.optimize()
+
+            # Get the solution vector
+            efm = np.array([v.x for v in m.vars[:n]])
+
+            # Check for optimality
+            if efm.any() is None:
+                break
+
+            # Add constraint to exclude the current solution in the next iteration
+            m += xsum(a[i] for i in supp(efm)) <= len(supp(efm)) - 1
+
+            efms.append(efm)
+
+        efms = np.array(efms)
+
+        # Separate positive and negative parts for reversible reactions
+        efms_p = efms[:, :len(rev)]
+        efms_m = np.zeros(np.shape(efms_p))
+
+        counter = 0
+        for r in supp(rev):
+            efms_m[:, r] = efms[:, len(rev) + counter]
+            counter += 1
+
+        efms = efms_p - efms_m
+
+        # Remove zero rows
+        efms = efms[np.any(efms != 0, axis=1)]
+
+        if only_reversible:
             self.efms = efms
-            return efms
-        
-        if algo == "milp":
-            efms = get_efms(self.stoich, self.rev,algo="milp")
-            self.efms = efms
-            return efms
-    
-    def get_rev_efms(self,algo = "efmtool"):
+        else:
+            self.rev_efms = efms
 
     def reversible_efms(self):
 
