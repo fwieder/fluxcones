04206dcced38c745e9e31553c9d250f1e69fa240
fwieder <59224157+fwieder@users.noreply.github.com>
2024-07-30 16:37:42 +0200

    added standardize_model function to helpers

--- fluxcones/helpers.py
+++ fluxcones/helpers.py
@@ -19,3 +19,35 @@ def abs_max(vector):
         return 0
     abs_max = np.max(np.absolute(vector[vector != 0]))
     return abs_max
+
+# Standardize model so that blocked reactions are deleted and irreversible reacctions are positively oriented
+def standardize_model(cobra_model):
+    
+    # this loop appends the indices of blocked reactions to "blocked reactions" and inverts negative irreversible reactions
+    
+    
+    toadd = []
+    todel = []
+    for rea in cobra_model.reactions:
+        
+        if rea.bounds == (0,0):
+            todel.append(rea)
+            #print(rea.id , "deleted")
+        if not rea.reversibility and rea.lower_bound < 0:
+           
+            reaction = cobra.Reaction(rea.id + "_inverted")
+            
+            reaction.lower_bound = -rea.upper_bound
+            reaction.upper_bound = -rea.lower_bound
+            metabs = {}
+            for key in rea.metabolites.keys():
+                metabs[key] = -rea.metabolites[key]
+            reaction.add_metabolites(metabs)
+            #print(rea.id , "inverted")
+            toadd.append(reaction)
+            todel.append(rea)
+            
+    cobra_model.add_reactions(toadd)
+    cobra_model.remove_reactions(todel)
+           
+    return cobra_model
